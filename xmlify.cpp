7257f5ef22511ab931a0de9d6eb824d92d4ad9ed
Chuck Jungmann <chuckj60@gmail.com>
2018-03-30 12:44:52 -0500

    Added -t option to call new print_table_schema() function to write a table schema.  Improved schema output (especially enum and set types).

--- xmlify.cpp
+++ xmlify.cpp
@@ -5,6 +5,17 @@
 
 using namespace mysqlcb;
 
+const char table_schema_query[] =
+   "SELECT COLUMN_NAME,"
+   " UPPER(DATA_TYPE), "
+   " CHARACTER_MAXIMUM_LENGTH, "
+   " COLUMN_TYPE,"
+   " NULLIF('0',INSTR(EXTRA,'auto_increment')) AS autoinc,"
+   " NULLIF('0',INSTR(COLUMN_KEY,'PRI')) AS prikey,"
+   " NULLIF(IS_NULLABLE,'YES') AS nullable"
+   " FROM COLUMNS"
+   " WHERE TABLE_SCHEMA=? AND TABLE_NAME=?";
+
 /** Adds attributes to a schema field element for set flags. */
 void add_field_attributes_from_flags(unsigned int flags)
 {
@@ -48,6 +59,188 @@ void print_schema(Binder &b)
       ++bd;
    }
    std::cout << "</schema>\n";
+}
+
+bool is_int_type(const Bind_Data &data_type)
+{
+   size_t len = get_size(data_type);
+   const char *str = static_cast<const char*>(data_type.data);
+   return 0==strncmp("INT", &str[len-3], 3);
+}
+
+bool is_enum_type(const Bind_Data &data_type)
+{
+   const char *str = static_cast<const char*>(data_type.data);
+   return 0==strncmp("ENUM", str, 4);
+}
+
+bool is_set_type(const Bind_Data &data_type)
+{
+   const char *str = static_cast<const char*>(data_type.data);
+   return 0==strncmp("SET", str, 3);
+}
+
+bool is_unsigned_type(const Bind_Data &column_type)
+{
+   size_t len = get_size(column_type);
+   if (len > 8)
+   {
+      const char *str = static_cast<const char*>(column_type.data);
+      return 0==strncmp("int", &str[len-8], 8);
+   }
+   else
+      return false;
+}
+
+/**
+ * Writes string to stdout, converting &"'<> to XML entities and handling double ' or "
+ *
+ * The character that *end points to should be the quoted string terminator or nullptr;
+ * When the function encounters either ' or ", it will compare it to the char at *end.
+ * If it matches, duplicates will be supressed. If not matched, adjacent ' or " will
+ * both print.
+ */
+void xmlify_sql_string(const char *start, const char *end=nullptr)
+{
+   if (end==nullptr)
+      end = start + strlen(start);
+
+   while(start<end)
+   {
+      switch(*start)
+      {
+         case '&':
+            std::cout << "&amp;";
+            break;
+         case '<':
+            std::cout << "&lt;";
+            break;
+         case '>':
+            std::cout << "&gt;";
+            break;
+         case '"':
+            std::cout << "&quot;";
+            break;
+         case '\'':
+            std::cout << "&apos;";
+            break;
+         default:
+            std::cout.put(*start);
+            break;
+      }
+
+      // Skip duplicate if it is string's enclosing character
+      if (*start==*end && *(start+1)==*end)
+         ++start;
+
+      ++start;
+   }
+}
+
+void add_value_list(const Bind_Data &column_type)
+{
+   const char *str = static_cast<const char *>(column_type.data);
+   const char *end = str + get_size(column_type);
+
+   const char *p = str;
+   const char *val = nullptr;
+
+   // Skip up to just-past the parenthesis.  Use the
+   // post-increment test for '(' to position pointer.
+   while(p < end && *p++!='(')
+      ;
+
+   while (p < end && *p == '\'')
+   {
+      val = ++p;
+
+      // Breaks out with terminating '
+      while (++p<end)
+      {
+         if (*p=='\'')         // terminating '?
+         {
+            if (*++p == '\'')  // ignore double 's
+               continue;
+            else
+            {
+               assert(*p==',' || *p==')');
+
+               std::cout << "   <item val=\"";
+               xmlify_sql_string(val, p-1); // send last ' as terminator
+               std::cout << "\" />\n";
+               val = nullptr;
+               
+               ++p;
+               break;
+            }
+         }
+      }
+   }
+}
+
+void print_columns_as_fields(const PullPack &pp)
+{
+   Bind_Data *bd = pp.binder.bind_data;
+   Bind_Data &bName     = bd[0];
+   Bind_Data &bDType    = bd[1];
+   Bind_Data &bCMaxLen  = bd[2];
+   Bind_Data &bCType    = bd[3];
+   Bind_Data &bAutoInc  = bd[4];
+   Bind_Data &bPriKey   = bd[5];
+   Bind_Data &bNullable = bd[6];
+
+   while(pp.puller(false))
+   {
+      std::cout << "<field name=\"" << bName << "\" type=\"" << bDType;
+
+      if (!is_null(bAutoInc))
+         std::cout << "\" auto_increment=\"true";
+      if (!is_null(bPriKey))
+         std::cout << "\" primary_key=\"true";
+      if (!is_null(bNullable))
+         std::cout << "\" not_null=\"true";
+      if (!is_null(bCMaxLen))
+         std::cout << "\" length=\"" << bCMaxLen;
+
+      // This must come just before the end because
+      // if it's a enum or set type, the values will
+      // be rendered as child nodes, which means no
+      // attributes can follow it.
+      if (is_enum_type(bDType) || is_set_type(bDType))
+      {
+         std::cout << "\">\n";
+         add_value_list(bCType);
+         std::cout << "</field>\n";
+      }
+      else
+      {
+         if (is_int_type(bDType) && is_unsigned_type(bCType))
+            std::cout << "\" unsigned=\"true";
+
+         std::cout << "\" />\n";
+      }
+   }
+}
+
+void print_table_schema(const char *host,
+                        const char *user,
+                        const char *pass,
+                        const char *dbase,
+                        const char *tname)
+{
+   auto f = [&dbase, &tname](MYSQL &mysql)
+   {
+      MParam params[3] = { dbase, tname };
+      std::cout << "<?xml version=\"1.0\" ?>\n<resultset>\n";
+      std::cout << "<schema name=\"" << tname << "\">\n";
+
+      execute_query_pull(mysql, print_columns_as_fields, table_schema_query, params);
+
+      std::cout << "</schema>\n";
+      std::cout << "</resultset>\n";
+   };
+
+   start_mysql(f,host,user,pass,"information_schema");
 }
 
 /**
@@ -100,7 +293,7 @@ void run_query(const char *query,
 void show_usage(void)
 {
    std::cout << "Usage instructions:\n\n"
-      "xmlify [-u USER] [-pPASSWORD] [-h HOST] [-e SQL_STATEMENT] [-s] database_name\n\n"
+      "xmlify [-u USER] [-pPASSWORD] [-h HOST] [-e SQL_STATEMENT] [-s] [-t tablename] database_name\n\n"
       "Options:\n"
       "-e sql statement\n"
       "   SQL statement that should be executed.\n"
@@ -113,6 +306,8 @@ void show_usage(void)
       "   Password for user account.  Note there is no space between -p and the password value.\n"
       "-s\n"
       "   Include a schema in the output.\n"
+      "-t tablename\n"
+      "   Output named table's schema.\n"
       "-u [user]\n"
       "   User account to use for the connection.\n"
       "\n"
@@ -130,6 +325,7 @@ int main(int argc, char **argv)
    const char *host = "localhost";
    const char *user = nullptr;
    const char *password = nullptr;
+   const char *tablename = nullptr;
    bool include_schema = 0;
    bool display_usage = 0;
 
@@ -165,6 +361,9 @@ int main(int argc, char **argv)
             case 's':
                include_schema = 1;
                break;
+            case 't':
+               tablename = argv[++i];
+               break;
             case 'u':
                user = argv[++i];
                break;
@@ -180,6 +379,10 @@ int main(int argc, char **argv)
 
    if (display_usage)
       show_usage();
+   else if (dbase && tablename)
+   {
+      print_table_schema(host, user, password, dbase, tablename);
+   }
    else
    {
       try
